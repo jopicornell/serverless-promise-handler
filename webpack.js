ffddbb398ffa2eb26df65725e3fa158cfa783ab6
Joan Picornell <joan.picornell@gmail.com>
2017-08-15 21:39:04 -0500

    first functional commit

---
+++ webpack.js
@@ -0,0 +1,48 @@
+const path = require('path');
+require('shelljs/global')
+var ora = require('ora')
+var webpack = require('webpack')
+
+console.log(
+  '  Tip:\n' +
+  '  Built files are meant to be served over an HTTP server.\n' +
+  '  Opening index.html over file:// won\'t work.\n'
+)
+
+var spinner = ora('building for production...')
+spinner.start()
+
+const webpackConfig = {
+  entry: './lib/index.js',
+  target: 'node',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
+};
+
+webpack(webpackConfig, function (err, stats) {
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})
